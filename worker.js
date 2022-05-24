5a9716ae81e0fb3969611fda683563b9c21e05fc
Amir Tadrisi <amirtds@gmail.com>
2022-05-24 13:12:36 -0500

    feat: adding pandas to the worker

--- worker.js
+++ worker.js
@@ -7,12 +7,41 @@ async function initialize(){
 
 let initialized = initialize();
 
+
 self.onmessage = async function(e){
     await initialized;
-    let result = await self.pyodide.runPythonAsync(`
-        def say_hello():
-            return "Hello from Python!"
-        say_hello()
+    let titles_list = await self.pyodide.runPythonAsync(`
+        import pandas as pd
+        from pyodide.http import pyfetch
+        response = await pyfetch("https://raw.githubusercontent.com/amirtds/kaggle-netflix-tv-shows-and-movies/main/titles.csv")
+        if response.status == 200:
+            with open("titles.csv", "wb") as f:
+                f.write(await response.bytes())
+        # 1. loading the csv file
+        all_titles = pd.read_csv("titles.csv")
+        # 2. Sanitizing the data
+        # Drop unnecessary columns
+        all_titles = all_titles.drop(
+            columns=[
+                "age_certification",
+                "seasons",
+                "imdb_id",
+            ]
+        )
+        # Drop rows with null values for important columns
+        sanitized_titles = all_titles.dropna(
+            subset=[
+                "id",
+                "title",
+                "release_year",
+                "genres",
+                "production_countries",
+                "imdb_score",
+                "imdb_votes",
+                "tmdb_score",
+            ]
+        )
+        sanitized_titles.head(10).to_json(orient="table")
     `);
-    self.postMessage(result);
+    self.postMessage(titles_list);
 }
