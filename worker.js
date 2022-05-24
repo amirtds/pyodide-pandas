3a01456750e16ab122ed16ca3942ac851ee202aa
Amir Tadrisi <amirtds@gmail.com>
2022-05-24 12:19:25 -0500

    chore: add pyodide to the worker

--- worker.js
+++ worker.js
@@ -1,6 +1,18 @@
-self.onmessage = function(e) {
-    console.log("hello from the worker")
-    console.log(e.data)
-    self.postMessage("hello from the worker")
+importScripts("https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js");
 
+async function initialize(){
+    self.pyodide = await loadPyodide();
+    await self.pyodide.loadPackage("pandas");
+}
+
+let initialized = initialize();
+
+self.onmessage = async function(e){
+    await initialized;
+    let result = await self.pyodide.runPythonAsync(`
+        def say_hello():
+            return "Hello from Python!"
+        say_hello()
+    `);
+    self.postMessage(result);
 }
